c1aa3ad0c6e9d363a2afbe92495c6ca1a097d00c
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-06-03 16:34:13 +0200

    add homePage rendering function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './css/style.css';
+import pizzaImg from './images/pizza-home.jpg';
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+const content = $('#content');
 
 
+const homePage = `
+  <div class="home">
+    <div>
+      <h2>„Smak Sycylii: Autentyczne smaki, rodzinna atmosfera”</h2>
+      <p>Poczuj ciepło południowych Włoch w każdym kęsie — ręcznie wyrabiana pizza, świeże składniki i sycylijska tradycja prosto na Twoim stole.</p>
+    </div>
+
+    <img src="${pizzaImg}" alt="Pizza">
+  </div>
+`;
+
+
+
+function renderHome() {
+  content.innerHTML = homePage;
+}
+
+renderHome();

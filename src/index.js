101bb90aa622ba6c04c51657946bca05166a589b
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-06-04 02:30:33 +0200

    add event listener to logo

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import homePage from './pages/home.js';
 
 const content = document.querySelector('#content');
 const navButtons = document.querySelectorAll('nav button');
+const logo = document.querySelector('.logo-wrapper');
 
 const pages = {
   homePage,
 }
+
+logo.addEventListener('click', () => {
+  renderPage(homePage);
+})
 
 navButtons.forEach(button => {
   button.addEventListener('click', () => {

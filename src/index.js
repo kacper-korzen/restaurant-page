fe9de88a67a77c203dc95830c53574e5fb44aa68
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-06-04 12:14:30 +0200

    add menu page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/style.css';
 import homePage from './pages/home.js';
+import menuPage from './pages/menu.js';
 
 const content = document.querySelector('#content');
 const navButtons = document.querySelectorAll('nav button');
@@ -8,6 +9,7 @@ const logo = document.querySelector('.logo-wrapper');
 
 const pages = {
   home : homePage,
+  menu : menuPage,
 }
 
 logo.addEventListener('click', () => {

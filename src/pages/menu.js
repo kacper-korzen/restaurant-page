ba17abf0e1d46afb5db6ee0d1c252ec7b5d686b5
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-06-04 13:03:10 +0200

    rewrite functions with dom manipulations

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,19 +2,23 @@ import favPizza1 from '../images/fav-pizza-1.jpg';
 import favPizza2 from '../images/fav-pizza-2.jpg';
 import favDessert from '../images/deser.jpg';
 
-function card(img, name, price) {
+function card(imgName, name, price) {
   const card = document.createElement('div');
   card.classList.add('card', 'grid');
 
-  const cardContent = `
-    <div>
-      <img src="${img}" alt="Pizza">
-      <h3>${name}</h3>
-      <p>${price}</p>
-    </div>
-  `;
+  const img = document.createElement('img');
+  img.src = imgName;
 
-  card.innerHTML = cardContent.toString();
+  const h3 = document.createElement('h3');
+  h3.textContent = name;
+
+  const p = document.createElement('p');
+  p.textContent = price;
+
+  card.appendChild(img);
+  card.appendChild(h3);
+  card.appendChild(p);
+
   return card;
 }
 
@@ -22,22 +26,27 @@ function cardsSection() {
   const section = document.createElement('section');
   section.classList.add('grid', 'centered-grid');
 
-  const cardsSection = `
-    <div>
-      <h2>Menu</h2>
-      <p>Każdy posiłek jest starannie przygotowywany,
-      aby zapewnić niezapomniane doznania kulinarne.</p>
-    </div>
+  const header = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
 
-    <div class="grid products">
-      ${card(favPizza1, 'Margherita', '10 PLN').innerHTML}
-      ${card(favPizza2, 'Bambinia', '12 PLN').innerHTML}
-      ${card(favDessert, 'Deser', '5 PLN').innerHTML}
-    </div>
-  `;
+  h2.textContent = 'Menu';
+  p.textContent = `Każdy posiłek jest starannie przygotowywany,
+  aby zapewnić niezapomniane doznania kulinarne.`;
+
+  header.appendChild(h2);
+  header.appendChild(p);
+
+  const cards = document.createElement('div');
+  cards.classList.add('grid', 'cards-wrapper');
+
+  cards.appendChild(card(favPizza1, 'Margherita', '10 PLN'));
+  cards.appendChild(card(favPizza2, 'Bambinia', '12 PLN'));
+  cards.appendChild(card(favDessert, 'Deser', '5 PLN'));
 
 
-  section.innerHTML = cardsSection;
+  section.appendChild(header);
+  section.appendChild(cards);
 
   return section;
 }
@@ -49,4 +58,6 @@ function menuPage(element) {
   return element;
 }
 
+
 export default menuPage;
+
